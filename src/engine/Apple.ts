ae5f670dd8dff0b8408fb47004af93e31e5bb5bd
heliandro <heliandro.l@gmail.com>
2024-03-10 22:56:53 -0300

    feat: add feature de items ao game

---
+++ src/engine/Apple.ts
@@ -0,0 +1,35 @@
+import { CanvasDimension, CanvasGridSize } from "./config";
+import { Color } from "./enums/Color";
+import { FillOptions } from "./utils";
+
+export default class Apple {
+
+    private x: number;
+    private y: number;
+    private width: number;
+    private height: number;
+
+
+    constructor() {
+        this.x = Math.floor(Math.random() * ((CanvasDimension.width) / CanvasGridSize)) * CanvasGridSize;
+        this.y = Math.floor(Math.random() * ((CanvasDimension.height) / CanvasGridSize)) * CanvasGridSize;
+        this.width = CanvasGridSize;
+        this.height = CanvasGridSize;
+    }
+
+    public getData(): { fillOptions: FillOptions, color: Color } {
+        return {
+            fillOptions: {
+                x: this.x,
+                y: this.y,
+                width: this.width,
+                height: this.height
+            },
+            color: Color.red
+        }
+    }
+
+    public equals(item: Apple) {
+        return this.x === item.getData().fillOptions.x && this.y === item.getData().fillOptions.y;
+    }
+}
---
+++ src/engine/Items.ts
@@ -0,0 +1,55 @@
+import Apple from "./Apple";
+import { PaintBrush } from "./utils";
+
+export default class Items {
+
+    private apples: Apple[] = [];
+    private appleQtd: number = 3;
+
+    private paintBrush: PaintBrush;
+
+    constructor(context: CanvasRenderingContext2D) {
+        this.paintBrush = new PaintBrush(context);
+        this.createItems(this.appleQtd);
+    }
+
+    createItems(quantity: number): Apple[] {
+        let i = 0;
+        while (i < quantity) {
+            let apple = new Apple();
+
+            if (i > 0 && this.isAppleColidedWithAnother(apple)) {
+                continue;
+            }
+
+            i++;
+            this.apples.push(apple);
+        }
+
+        return this.apples;
+    }
+
+    draw() {
+        this.apples.forEach(apple => {
+            this.paintBrush.drawPixel(apple.getData().fillOptions, apple.getData().color);
+        })
+    }
+
+    isAppleColidedWithAnother(colidedApple: Apple) {
+        return this.apples.some(apple => apple.equals(colidedApple));
+    }
+
+    removeItem(colidedApple: Apple) {
+        this.apples = this.apples.filter(apple => !apple.equals(colidedApple));
+    }
+
+    getApples(): Apple[] {
+        return this.apples;
+    }
+
+    createNewApplesWithSnakeEatAll() {
+        if (this.apples.length === 0) {
+            this.createItems(this.appleQtd);
+        }
+    }
+}
